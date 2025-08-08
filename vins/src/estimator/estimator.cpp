e2e86d7e52ebb59b076c8bc66b972b3683ce2ef8
Youssef Darahem <youssef.darahem@gmail.com>
2025-08-08 14:11:23 +0300

    Updated to run on Ros2 humble

--- vins/src/estimator/estimator.cpp
+++ vins/src/estimator/estimator.cpp
@@ -1168,7 +1168,8 @@ void Estimator::optimization()
 
     if (USE_GPU_CERES)
         // std::cout << "1" << endl;
-        options.dense_linear_algebra_library_type = ceres::CUDA;
+        // options.dense_linear_algebra_library_type = ceres::CUDA;
+        options.linear_solver_type = ceres::DENSE_SCHUR;
     else
         // std::cout << "2" << endl;
         options.linear_solver_type = ceres::DENSE_SCHUR;
